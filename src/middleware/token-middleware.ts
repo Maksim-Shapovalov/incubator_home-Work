3ca664c7d574380e822e07728cd73431b0de119e
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2023-12-05 18:46:08 +0300

    first commit - 65

--- src/middleware/token-middleware.ts
+++ src/middleware/token-middleware.ts
@@ -31,16 +31,33 @@ export const ValidationRefreshToken = async (req: Request, res: Response , next:
             res.sendStatus(HTTP_STATUS.UNAUTHORIZED_401)
             return
         }
-        const device = await securityDevicesRepo.getDevice(payload.deviceId,user._id.toString())
-        if(!device || device === 5){
-            res.sendStatus(HTTP_STATUS.UNAUTHORIZED_401)
-            return
-        }
 
         req.body.user = user
         req.body.deviceId = payload
 
 
+        next()
+        return;
+    }else{
+
+        return res.sendStatus(HTTP_STATUS.UNAUTHORIZED_401)
+    }
+}
+export const FindDeviceInDBToRefreshToken = async (req: Request, res: Response , next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken
+    const payload = await jwtService.parseJWTRefreshToken(refreshToken);
+    if (payload){
+        const userId = new ObjectId(payload.userId) ;
+        const user = await userRepository.getUserById(userId)
+        if(!user){
+            res.sendStatus(HTTP_STATUS.UNAUTHORIZED_401)
+            return
+        }
+        const device = await securityDevicesRepo.getDevice(payload.deviceId,user._id.toString())
+        if(!device || device === 5){
+            res.sendStatus(HTTP_STATUS.UNAUTHORIZED_401)
+            return
+        }
         next()
         return;
     }else{
